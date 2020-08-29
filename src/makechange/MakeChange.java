b8fe18f73d713d2aea89f06045a9a0d733a29513
Tj Laughlin <tlaughl1@gmail.com>
2020-08-29 17:17:13 -0600

    need to fix print out for price == tendered

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -6,14 +6,14 @@ public class MakeChange {
 
 	public static void main(String[] args) {
 
-//		double twenty = 0;
-//		double ten = 0;
-//		double five = 0;
-//		double one = 0;
-//		double quarter = 0;
-//		double dime = 0;
-//		double nickel = 0;
-//		double penny = 0;
+		double twenty = 0;
+		double ten = 0;
+		double five = 0;
+		double one = 0;
+		double quarter = 0;
+		double dime = 0;
+		double nickel = 0;
+		double penny = 0;
 
 		Scanner kb = new Scanner(System.in);
 		System.out.println("What is the price of your item? ");
@@ -23,58 +23,73 @@ public class MakeChange {
 		double tendered = kb.nextDouble();
 		double change = (tendered + .00005) - price; 
 		
-		if (tendered < price) {
-			System.out.println("the amount you have given is not enough, please provide enough money for your item");
-			customerPrompt();
-		} else if (tendered == price) {
-			System.out.println("you have provided the exact amount, thank you for coming and have a grat day! ");
-		}
-
 		int numOf20s = (int) change / 20;
 		int numOf10s = (int) change / 10;
 		int numOf5s = (int) change / 5;
 		int numOf1s = (int) change / 1;
-
+		double numOfQs = (int) (change / .25);
+		double numOfDs = (int) (change / .1);
+		double numOfNs = (int) (change / .05);
+		double numOfPs = (int) (change / .01);
+		
+		if (price == tendered) {
+			System.out.println("you have provided the exact amount, thank you for coming and have a great day! ");
+			
+		}
+	while (tendered < price)	
+		if (tendered < price) {
+			System.out.println("the amount you have given is not enough, please provide enough money for your item");
+			System.out.println("What is the price of your item? ");
+			price = kb.nextDouble();
+			System.out.println("What is the amount tendered? ");
+			kb.nextLine();
+			tendered = kb.nextDouble();
+			change = (tendered + .00005) - price; 
+		} 
 		
 
-		while (change > 0) {
+	
+	System.out.println("Your change is: ");
+		
+	while (change > 0) {
 			
 			if ((int) change / 20 > 0) {
 				numOf20s = (int) change / 20;
-				System.out.print("your change will be " + numOf20s + " twentys, ");
-				System.out.println(change);
+				System.out.print(numOf20s + " twenty(s), ");
 				change -= numOf20s * 20;
 			} else if ((int) change / 10 > 0) {
 				numOf10s = (int) change / 10;
-				System.out.print("your change will be " + numOf10s + " tens, ");
+				System.out.print(numOf10s + " ten(s), ");
 				change -= numOf10s * 10;
 			} else if ((int) change / 5 > 0) {
 				numOf5s = (int) change / 5;
-				System.out.print("your change will be " + numOf5s + " fives, ");
+				System.out.print(numOf5s + " five(s), ");
 				change -= numOf5s * 5;
 			} else if ((int) change / 1 > 0) {
 				numOf1s = (int) change / 1;
-				System.out.print("your change will be " + numOf1s + " ones, ");
+				System.out.print(numOf1s + " one(s), ");
 				change -= numOf1s;
 			} else if (change > .25) {
-				double numOfQs = (int) (change / .25);
-				System.out.print("your change will be " + numOfQs + " quarters, ");
+				numOfQs = (int) (change / .25);
+				System.out.print(numOfQs + " quarter(s), ");
 				change -= numOfQs * (.25);
 			} else if (change > .1) {
-				double numOfDs = (int) (change / .1);
-				System.out.print("your change will be " + numOfDs + " dimes, ");
+				numOfDs = (int) (change / .1);
+				System.out.print(numOfDs + " dime(s), ");
 				change -= numOfDs * (.1);
 			} else if (change > .05) {
-				double numOfNs = (int) (change / .05);
-				System.out.println("your change will be " + numOfNs + " nickels");
+				numOfNs = (int) (change / .05);
+				System.out.print(+ numOfNs + " nickel(s) ");
 				change -= numOfNs * (.05);
 			} else if (change > .01) {
-				double numOfPs = (int) (change / .01);
-				System.out.println("your change will be " + numOfPs + " pennies.");
+				numOfPs = (int) (change / .01);
+				System.out.print(+ numOfPs + " pennie(s)");
 				change -= numOfPs;
 			}
 
 		}
+		
+		
 	}
 
 	public static void customerPrompt() {
@@ -97,39 +112,40 @@ public class MakeChange {
 			
 			if ((int) change / 20 > 0) {
 				numOf20s = (int) change / 20;
-				System.out.print("your change will be " + numOf20s + " twentys, ");
+				System.out.print(numOf20s + " twenty(s), ");
 				System.out.println(change);
 				change -= numOf20s * 20;
 			} else if ((int) change / 10 > 0) {
 				numOf10s = (int) change / 10;
-				System.out.print("your change will be " + numOf10s + " tens, ");
+				System.out.print(numOf10s + " ten(s), ");
 				change -= numOf10s * 10;
 			} else if ((int) change / 5 > 0) {
 				numOf5s = (int) change / 5;
-				System.out.print("your change will be " + numOf5s + " fives, ");
+				System.out.print(numOf5s + " five(s), ");
 				change -= numOf5s * 5;
 			} else if ((int) change / 1 > 0) {
 				numOf1s = (int) change / 1;
-				System.out.print("your change will be " + numOf1s + " ones, ");
+				System.out.print(numOf1s + " one(s), ");
 				change -= numOf1s;
 			} else if (change > .25) {
 				double numOfQs = (int) (change / .25);
-				System.out.print("your change will be " + numOfQs + " quarters, ");
+				System.out.print(numOfQs + " quarters(), ");
 				change -= numOfQs * (.25);
 			} else if (change > .1) {
 				double numOfDs = (int) (change / .1);
-				System.out.print("your change will be " + numOfDs + " dimes, ");
+				System.out.print(numOfDs + " dime(s), ");
 				change -= numOfDs * (.1);
 			} else if (change > .05) {
 				double numOfNs = (int) (change / .05);
-				System.out.println("your change will be " + numOfNs + " nickels");
+				System.out.print(numOfNs + " nickel(s) ");
 				change -= numOfNs * (.05);
 			} else if (change > .01) {
 				double numOfPs = (int) (change / .01);
-				System.out.println("your change will be " + numOfPs + " pennies.");
+				System.out.print(numOfPs + " pennie(s).");
 				change -= numOfPs;
 			}
 
 		}
 	}
+	
 }
